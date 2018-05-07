dec5197173c36e9f440aceaaadefe0dd58529a70
Cornelia Smith <cornelia.q.smith@gmail.com>
2018-05-07 17:52:45 -0500

    Show ingredients#show page when creation is successful

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'ingredients/new'
   post 'ingredients/create'
   get 'ingredients' => 'ingredients#index', as: :ingredients
-  get '/ingredients/:id', to: 'ingredients#show'
+  get '/ingredients/:id', to: 'ingredients#show', as: :ingredient
 
   get 'recipes/new'
   post 'recipes/create'
