dcf21a5224a9e9958272f03c50129787356e4897
Cornelia Smith <cornelia.q.smith@gmail.com>
2018-06-21 14:34:29 -0500

    Prevent users from deleting ingredients that are in recipes

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredient_joins
   has_many :recipes, :through => :recipe_ingredient_joins
+
+  def in_recipe?
+    recipes.size > 0
+  end
 end
