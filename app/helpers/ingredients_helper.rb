e59cfddb52ba37b7afca6caedf8b33cce50e8a07
Cornelia Smith <cornelia.q.smith@gmail.com>
2018-06-21 14:06:43 -0500

    WIP: add helper method

--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -1,2 +1,9 @@
 module IngredientsHelper
+
+def is_ingredent_used #come up with a better name
+  # Look up ingredient in recipe_ingredients by id param
+  # return true or false
+  # another method to enable/disable button and show tooltip
+end
+
 end
