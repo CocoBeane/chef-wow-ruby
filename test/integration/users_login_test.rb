fa3c0dd82aacda4248218ee74adc0c84c3771473
Cornelia Smith <cornelia.q.smith@gmail.com>
2018-07-30 16:57:41 -0500

    Users can only create recipes and ingredients if logged in

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:test)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -11,4 +15,58 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get signup_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: '123456' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "new ingredient path only shows if logged in" do
+    get login_path
+    assert_select "a[href=?]", 'ingredients/new', count: 0
+    post login_path, params: { session: { email:    @user.email,
+                                          password: '123456' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", '/ingredients/new'
+  end
+
+  test "new recipe path only shows if logged in" do
+    get login_path
+    assert_select "a[href=?]", 'recipes/new', count: 0
+    post login_path, params: { session: { email:    @user.email,
+                                          password: '123456' } }
+    assert_redirected_to @user
+    follow_redirect!                                      
+    assert_select "a[href=?]", '/recipes/new'
+  end
+
+  test "new ingredient page only accessible if logged in" do
+    get '/ingredients/new'
+    assert_select "form", false
+    assert_select "div.alert-danger"
+    post login_path, params: { session: { email:    @user.email,
+                                          password: '123456' } }
+    get '/ingredients/new'                                   
+    assert_select "form"
+    assert_select "div.alert-danger", false
+  end
+
+  test "new recipe page only accessible if logged in" do
+    get '/recipes/new'
+    assert_select "form", false
+    assert_select "div.alert-danger"
+    post login_path, params: { session: { email:    @user.email,
+                                          password: '123456' } }
+    get '/recipes/new'                                    
+    assert_select "form"
+    assert_select "div.alert-danger", false
+  end
+
 end
