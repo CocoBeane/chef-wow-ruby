86d56f36861e600ea97102ce15825a5013d9491d
Cornelia Smith <cornelia.q.smith@gmail.com>
2018-10-01 10:59:58 -0500

    Users can delete their accounts

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,16 +37,41 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@second_user)
     get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@second_user)
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
-    assert_not flash.empty?
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should not allow the admin attribute to be updated via web" do
+    log_in_as @user
+    assert_not @user.admin?
+    patch user_path(@user), params: { user: {
+                                      password: "",
+                                      password_confirmation: "",
+                                      admin: true } }
+    assert_not @user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in and not admin" do
+    log_in_as(@user)
+    assert_no_difference 'User.count' do
+      delete user_path(@second_user)
+    end
+    assert_redirected_to root_url
   end
 
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,17 +4,33 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:test)
+    @admin_user = users(:bob)
   end
 
-  test "index including pagination" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin_user)
     get users_path
     assert_template 'users/index'
     assert_select 'nav.pagination', count: 2
     
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin_user
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@user)
+    end
+  end
+
+  test "Index as non-admin does not include delete link" do
+    log_in_as(@user)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 
 end
